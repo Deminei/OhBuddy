37295589d3a60476337d0a95878d3d320b0461a9
chitragoenka <chitra.goenka@gmail.com>
2023-05-02 14:31:10 -0400

    added error handling

--- chitra.js
+++ chitra.js
@@ -10,3 +10,26 @@ deletePostButtons.forEach(function(button) {
     post.remove();
   });
 });
+
+function deletePostById(posts, id) {
+  let postFound = false;
+  for (let i = 0; i < posts.length; i++) {
+    if (posts[i].id === id) {
+      posts.splice(i, 1); // Remove the post from the array
+      postFound = true;
+      break;
+    }
+  }
+  if (!postFound) {
+    throw new Error(`Post with id ${id} not found`);
+  }
+}
+
+const posts = JSON.parse(localStorage.getItem('posts'));
+
+try {
+  deletePostById(posts, 123); // Delete post with id 123
+  localStorage.setItem('posts', JSON.stringify(posts)); // Save updated posts to local storage
+} catch (error) {
+  console.error(error); // Log the error message
+}
