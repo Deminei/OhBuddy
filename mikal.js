468002dfb08a1e5611f4e5e465191c49d4c49694
~ Deminei ~ <wowinfoonly1@gmail.com>
2023-05-03 10:40:07 -0400

    Makeover time

--- mikal.js
+++ mikal.js
@@ -1,14 +1,14 @@
 function generatePostHtml(post) {
   return `
-      <div class="card right" id="${post.id}"style="width: 18rem;">
+      <div class="card" id="${post.id}">
         <div class="card-body">
           <h5 class="card-title">${post.name}</h5>
-          <h6 class="card-subtitle mb-2 text-muted">${post.date}</h6>
+          <h6 class="card-subtitle mb-2 text-muted"><sub>${post.date}</sub></h6>
           <p class="card-text">${post.post}</p>
           <label for="">Tags: </label>
-          <span class="tags">${post.tags}</span>
+          <a href="#" class="tags">${post.tags}</a>
           <div>
-              <a href="#" class="card-link">Update</a>
+              <a href="#" class="card-link update-btn">Update</a>
             <a href="#" class="card-link delete-btn">Delete</a>
             </div>
         </div>
